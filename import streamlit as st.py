63e65a947082958af6290c713c0eeae00a0545f2
Luul1649 <luulhaji1649@gmail.com>
2025-07-21 12:16:52 +0300

    Add files via upload

---
+++ import streamlit as st.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Title
+st.title("CArbon Emissions in Kenya Over the Years")
+
+# Upload CSV
+uploaded_file = st.file_uploader("C:\\Users\\hi\\Downloads\\cleaned energy dataset.csv")
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    st.success("File successfully uploaded!")
+
+    # Show dataset
+    st.subheader("Raw Dataset")
+    st.write(df)
+
+    # Filter by Year Range
+    years = df["Year"]
+    min_year, max_year = int(years.min()), int(years.max())
+    start_year, end_year = st.slider("Select Year Range", min_value=min_year, max_value=max_year, value=(min_year, max_year))
+
+    filtered_df = df[(df["year"] >= start_year) & (df["year"] <= end_year)]
+
+    # Line Chart
+    st.subheader("GHG Emissions Trend")
+    fig, ax = plt.subplots()
+    ax.plot(filtered_df["year"], filtered_df["ghg_Emissions"], marker='o')
+    ax.set_xlabel("year")
+    ax.set_ylabel("CARBON Emissions")
+    ax.set_title("CARBON EMISSIONS vs year")
+    st.pyplot(fig)
+
+    # Download Filtered Data
+    st.download_button("Download Filtered Data", filtered_df.to_csv(index=False), file_name="filtered_emissions.csv")
